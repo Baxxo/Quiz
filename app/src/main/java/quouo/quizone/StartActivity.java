4d6c6505348d630c00e30720901dc24c5fda3de7
Baxxo <matteobasso9@gmail.com>
2016-05-27 20:04:27 +0200

    il login resta false

--- app/src/main/java/quouo/quizone/StartActivity.java
+++ app/src/main/java/quouo/quizone/StartActivity.java
@@ -19,22 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Handler;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Date;
-
 public class StartActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
     Boolean con;
-    Boolean accesso = false;
+    Boolean accesso;
     EditText et1;
     EditText et2;
     TextView t1;
@@ -43,34 +32,25 @@ public class StartActivity extends AppCompatActivity {
     Button registrati;
     String user;
     Dialog d;
-    String ris;
+    Dialog l;
+    String ris = new String();
     ConnectionHandler hand = new ConnectionHandler();
-    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-    SharedPreferences.Editor editor = preferences.edit();
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
         setContentView(R.layout.activity_start);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        editor = preferences.edit();
 
         d = new Dialog(this);
+        l = new Dialog(this);
 
-        editor.putString("Accesso", String.valueOf(accesso));
-        editor.apply();
-
-        String acc = preferences.getString("Accesso", "false");
-        if (!acc.equalsIgnoreCase("")) {
-            acc = acc;
-        }
-        makeToast(acc);
-
-        if (accesso == true){
-            et1.setVisibility(View.INVISIBLE);
-            et2.setVisibility(View.INVISIBLE);
-            t1.setVisibility(View.INVISIBLE);
-            t2.setVisibility(View.INVISIBLE);
-        }
+        accesso = Boolean.getBoolean(preferences.getString("Accesso", "null"));
+        makeToast("fuori:" + accesso);
 
         if (!hasConnection()) {
             //con = false;
@@ -91,105 +71,160 @@ public class StartActivity extends AppCompatActivity {
             });
         }
 
+        if (accesso == false) {
 
-        et1 = (EditText) findViewById(R.id.editText);
-        et2 = (EditText) findViewById(R.id.editText2);
-        t1 = (TextView) findViewById(R.id.textView4);
-        t2 = (TextView) findViewById(R.id.textView5);
-        accedi = (Button) findViewById(R.id.button2);
-        registrati = (Button) findViewById(R.id.button3);
+            et1 = (EditText) findViewById(R.id.editText);
+            et2 = (EditText) findViewById(R.id.editText2);
+            t1 = (TextView) findViewById(R.id.textView4);
+            t2 = (TextView) findViewById(R.id.textView5);
+            accedi = (Button) findViewById(R.id.button2);
+            registrati = (Button) findViewById(R.id.button3);
 
-        accedi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(accesso == false){
-                    accesso = true;
-                }
-                editor.putString("nome", String.valueOf(et1.getText()));
-                editor.putString("pass", String.valueOf(et2.getText()));
-                editor.apply();
-                ris = hand.Login(preferences.getString("nome", "false")  ,preferences.getString("pass", "false"));
+            accedi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editor.putString("nome", String.valueOf(et1.getText()));
+                    editor.putString("pass", String.valueOf(et2.getText()));
+                    editor.apply();
+                    ris = hand.Login(preferences.getString("nome", "vuoto"), preferences.getString("pass", "vuoto"));
 
-                if (ris.equals("FAILED")) {
+                    if (ris.equals("FAILED")) {
 
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            makeToast("login " + ris);
-                        }
-                    }, 1);
-
-                } else {
-
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            makeToast("login " + ris);
-                        }
-                    }, 1);
-
-                    user = String.valueOf(et1.getText());
-                    if (con == true) {
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
-                                Player.nome = user;
-                                Player.id = Integer.valueOf(ris);
-                                startActivity(intent);
-                                finish();
+                                makeToast("login " + ris);
                             }
+                        }, 1);
 
+                    } else {
 
-                        }, 1000);
-                    }
-                }
-            }
-        });
-
-        registrati.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ris = hand.Registrazione(String.valueOf(et1.getText()), String.valueOf(et2.getText()));
-                if (ris.equals("FAILED")) {
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            makeToast("registration " + ris);
-                        }
-                    }, 1);
-                }
-                if (ris.equals("USER ALREADY EXISTS")) {
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            makeToast("registration " + ris);
-                        }
-                    }, 1);
-                }
-                if (ris.equals("SUCCESS")) {
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            makeToast("registration " + ris);
-                        }
-                    }, 1);
-                    user = String.valueOf(et1.getText());
-
-                    if (con == true) {
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
-                                intent.putExtra("User", user);
-
-                                startActivity(intent);
+                                makeToast("login " + ris);
                             }
-                        }, 1000);
+                        }, 1);
+
+                        user = String.valueOf(et1.getText());
+                        if (con == true) {/*
+                            d.setTitle("ciao");
+                            d.setCancelable(false);
+                            d.setContentView(R.layout.login);
+                            d.show();*/
+                            accesso = true;
+                            editor.putString("Accesso", "true");
+                            editor.apply();
+                            makeToast("funzione:" + preferences.getString("Accesso", "null"));
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                                    Player.nome = user;
+                                    Player.id = Integer.valueOf(ris);
+                                    startActivity(intent);
+                                    finish();
+                                }
+
+
+                            }, 1000);
+                        }
                     }
                 }
+            });
+
+
+            registrati.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ris = hand.Registrazione(String.valueOf(et1.getText()), String.valueOf(et2.getText()));
+                    if (ris.equals("FAILED")) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                makeToast("registration " + ris);
+                            }
+                        }, 1);
+                    }
+                    if (ris.equals("USER ALREADY EXISTS")) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                makeToast("registration " + ris);
+                            }
+                        }, 1);
+                    }
+                    if (ris.equals("SUCCESS")) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                makeToast("registration " + ris);
+                            }
+                        }, 1);
+                        user = String.valueOf(et1.getText());
+
+                        if (con == true) {
+                            accesso = true;
+                            editor.putString("Accesso", "true");
+                            editor.apply();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                                    intent.putExtra("User", user);
+                                    startActivity(intent);
+                                }
+                            }, 1000);
+                        }
+                    }
+                }
+            });
+        } else {
+
+            et1.setVisibility(View.INVISIBLE);
+            et2.setVisibility(View.INVISIBLE);
+            t1.setVisibility(View.INVISIBLE);
+            t2.setVisibility(View.INVISIBLE);
+            accedi.setVisibility(View.INVISIBLE);
+            registrati.setVisibility(View.INVISIBLE);
+
+            ris = hand.Login(preferences.getString("nome", "null"), preferences.getString("pass", "null"));
+
+            if (ris.equals("FAILED")) {
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeToast("login " + ris);
+                    }
+                }, 1);
+
+            } else {
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        makeToast("login " + ris);
+                    }
+                }, 1);
+
+                user = String.valueOf(et1.getText());
+                if (con == true) {
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                            Player.nome = user;
+                            Player.id = Integer.valueOf(ris);
+                            startActivity(intent);
+                            finish();
+                        }
+
+
+                    }, 1000);
+                }
             }
-        });
+        }
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
