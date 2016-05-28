cb355494b35f5f95d85aa5f57f5c21eb22f2811b
pellizzarigiacomo <giacomo.pellizzari4007@gmail.com>
2016-05-28 08:47:48 +0200

    gameOver

--- app/src/main/java/quouo/quizone/gameOver.java
+++ app/src/main/java/quouo/quizone/gameOver.java
@@ -6,17 +6,22 @@ import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class gameOver extends AppCompatActivity {
 
+    RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
         TextView textView = (TextView)findViewById(R.id.textView6);
-        textView.setText("Hai risposto su " + getIntent().getStringExtra("Punteggio") + "/5");
-        Button button = (Button)findViewById(R.id.button4);
+
+        float punteggio1 = Float.parseFloat(punteggio);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setRating(punteggio);
+        Button button = (Button)findViewById(R.id.back);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
